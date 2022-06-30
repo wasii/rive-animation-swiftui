c01a2fb36ce4281cbfc2b93bfe34e335c62bb7a0
abdWasiq <abdul.wasiq@naheed.pk>
2022-06-30 17:37:56 +0500

    tab barview initialised.

---
+++ RiveAnimation/RiveAnimation/Views/TabBarView.swift
@@ -0,0 +1,68 @@
+//
+//  TabBarView.swift
+//  RiveAnimation
+//
+//  Created by NaheedPK on 30/06/2022.
+//
+
+import SwiftUI
+import RiveRuntime
+
+struct TabBarView: View {
+    let icon = RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                content
+            }
+            .padding(12)
+            .background(Color("Background 2").opacity(0.8))
+            .background(.ultraThinMaterial)
+            .mask(
+                RoundedRectangle(cornerRadius: 24, style: .continuous)
+            )
+            .shadow(color: Color("Background 2").opacity(0.3), radius: 20, x: 0, y: 20)
+            .overlay(
+                RoundedRectangle(cornerRadius: 24, style: .continuous)
+                    .stroke(
+                        .linearGradient(colors: [.white.opacity(0.5), .white.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    )
+            )
+            .padding(.horizontal, 24)
+        }
+    }
+    
+    var content: some View {
+        ForEach(tabItems) { item in
+            Button {
+                try? item.icon.setInput("active", value: true)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    try? item.icon.setInput("active", value: false)
+                }
+            } label: {
+                item.icon.view()
+                    .frame(height: 36)
+            }
+        }
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
+
+struct TabItem: Identifiable {
+    var id = UUID()
+    var icon: RiveViewModel
+}
+
+var tabItems = [
+    TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")),
+    TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "SEARCH_Interactivity", artboardName: "SEARCH")),
+    TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "TIMER_Interactivity", artboardName: "TIMER")),
+    TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "BELL_Interactivity", artboardName: "BELL")),
+    TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "USER_Interactivity", artboardName: "USER"))
+]
