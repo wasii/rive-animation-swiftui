64502f8dcfa87d160b09b69e2eca6ce49fa64b73
abdWasiq <abdul.wasiq@naheed.pk>
2022-06-30 14:43:26 +0500

    Button Animation work done in Rive

--- RiveAnimation/RiveAnimation/OnboardingView.swift
+++ RiveAnimation/RiveAnimation/OnboardingView.swift
@@ -9,7 +9,34 @@ import SwiftUI
 import RiveRuntime
 
 struct OnboardingView: View {
+    let startCourseButton = RiveViewModel(fileName: "button")
     var body: some View {
+        ZStack {
+            ///Background Section
+            background
+            
+            
+            ///Button Section
+            startCourseButton.view()
+                .frame(width: 236, height: 64)
+                .overlay(
+                    Label("Start the Course", systemImage: "arrow.forward")
+                        .offset(x: 4, y: 4)
+                        .font(.headline)
+                )
+                .background(
+                    Color.black
+                        .cornerRadius(30)
+                        .blur(radius: 10)
+                        .opacity(0.3)
+                        .offset(y:10)
+                )
+                .onTapGesture {
+                    try? startCourseButton.play(animationName: "active")
+                }
+        }
+    }
+    var background: some View {
         RiveViewModel(fileName: "shapes").view()
             .ignoresSafeArea()
             .blur(radius: 30)
@@ -17,7 +44,7 @@ struct OnboardingView: View {
                 Image("Spline")
                     .blur(radius: 30)
                     .offset(x: 200, y: 100)
-            )
+        )
     }
 }
 
