3f8c3b50f82e9ccbb55e4e9867f06f098e47b3f2
abdWasiq <abdul.wasiq@naheed.pk>
2022-06-30 12:05:30 +0500

    project initialised, rive dependency integrated

---
+++ RiveAnimation/RiveAnimation/RiveAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveAnimationApp.swift
+//  RiveAnimation
+//
+//  Created by NaheedPK on 30/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct RiveAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
