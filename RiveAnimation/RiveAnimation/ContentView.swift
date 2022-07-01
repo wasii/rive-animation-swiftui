57ecf66bba88e2054fd93f0f98bf84bedf34b126
abdWasiq <abdul.wasiq@naheed.pk>
2022-07-01 15:09:21 +0500

    Side menu animationss...

--- RiveAnimation/RiveAnimation/ContentView.swift
+++ RiveAnimation/RiveAnimation/ContentView.swift
@@ -13,6 +13,13 @@ struct ContentView: View {
     let menuButton = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false)
     var body: some View {
         ZStack {
+            Color("Background 2")
+                .ignoresSafeArea()
+            
+            SideMenu()
+                .opacity(isOpen ? 1 : 0)
+                .offset(x: isOpen ? 0 : -300)
+                .rotation3DEffect(.degrees(isOpen ? 0 : 30), axis: (x: 0, y: 1, z: 0))
             Group {
                 switch selectedTab {
                 case .chat:
@@ -33,6 +40,12 @@ struct ContentView: View {
             .safeAreaInset(edge: .top) {
                 Color.clear.frame(height: 104)
             }
+            .mask(
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
+            )
+            .rotation3DEffect(.degrees(isOpen ? 30 : 0), axis: (x: 0, y: -1, z: 0))
+            .offset(x: isOpen ? 265 : 0)
+            .scaleEffect(isOpen ? 0.9 : 1)
             .ignoresSafeArea()
             menuButton.view()
                 .frame(width: 44, height: 44)
@@ -40,12 +53,15 @@ struct ContentView: View {
                 .shadow(color: Color("Background 2").opacity(0.2), radius: 5, x: 0, y: 5)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                 .padding()
+                .offset(x: isOpen ? 216 : 0)
                 .onTapGesture {
                     try? menuButton.setInput("isOpen", value: isOpen)
-                    isOpen.toggle()
-                    
+                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7)) {
+                        isOpen.toggle()
+                    }
                 }
             TabBarView()
+                .offset(y: isOpen ? 300 : 0)
         }
     }
 }
